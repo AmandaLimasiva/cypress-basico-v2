2a52535e1214af81978ae1c47be57c0361b7225d
Amanda Lima <amandalimasiva@gmail.com>
2022-07-11 19:47:47 -0300

    Subindo Casos de Teste Criados

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -104,4 +104,10 @@ describe('Central de Atendimento ao Cliente TAT', function(){
         cy.get('.success').should('be.visible')
     })
 
+    it.only('YouTube', function(){
+        cy.get('#product')
+            .select('YouTube')
+                .should('have.value', 'youtube')
+    })
+
 })
