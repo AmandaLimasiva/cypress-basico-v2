264434d193a5590c4ecd26a6dd9db977199c6849
Amanda Lima <amandalimasiva@gmail.com>
2022-07-17 20:59:08 -0300

    Subindo os testes finais

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -186,4 +186,16 @@ describe('Central de Atendimento ao Cliente TAT', function(){
             })
     })
 
+    it(' verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', function(){
+        cy.get('#privacy a').should('have.attr', 'target', '_blank')
+    })
+
+    it('acessa a página da política de privacidade removendo o target e então clicanco no link', function(){
+        cy.get('#privacy a')
+            .invoke('removeAttr', 'target')
+        cy.get('#privacy a').click()
+            cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT - Política de privacidade')
+    })
+
+
 })
